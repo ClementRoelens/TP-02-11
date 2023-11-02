78ec68ad05560dab3e754d3a0a59d626ed2bddb6
Nassim Sakhri <tails01@hotmail.fr>
2023-11-02 16:50:04 +0100

    configuration des differents parametres

--- app-back-end/app.js
+++ app-back-end/app.js
@@ -6,7 +6,70 @@ const port = 3000
 app.use(express.json())
 
 const recipeData = JSON.parse(fs.readFileSync('recipe.json'))
-const userData = JSON.parse(fs.readFileSync('user.json'))
 const ingredientData = JSON.parse(fs.readFileSync('ingredient.json'))
 
-app.get('')
+app.get('/recipes', (req, res) =>{
+    res.json(recipeData);
+})
+
+app.get('/recipe/:id', (req,res) => {
+    const recipeId = parseInt(req.params.id)
+    console.log(recipeData.recipes)
+    const recipe = recipeData.recipes.find(recipe => recipe.id === recipeId)
+    if (recipe){
+        res.json(recipe)
+    } else{
+        res.status(404).json({message : 'recette non trouvée'})
+    }
+})
+
+app.post('/recipes', (req,res) => {
+    const newRecipe = req.body
+    recipeData.recipes.push(newRecipe)
+    fs.writeFileSync('recipe.json', JSON.stringify(recipeData, null,2))
+    res.status(201).json(newRecipe)
+})
+
+
+
+
+
+
+
+
+
+app.get('/ingredients', (req, res) =>{
+    res.json(ingredientData);
+})
+
+app.get('/ingredient/:id', (req,res) => {
+    const ingredientId = req.params.id
+    const ingredient = ingredientData.ingredients.find(ingredient => ingredient.id === ingredientId)
+    if (ingredient){
+        res.json(ingredient)
+    } else{
+        res.status(404).json({message : 'ingredient non trouvée'})
+    }
+})
+
+app.post('/ingredients', (req,res) =>{
+    const newIngredient = parseInt(req.params.id)
+    ingredientData.ingredients.push(newIngredient)
+    fs.writeFileSync('ingredient.json', JSON.stringify(ingredientData, null, 2))
+    res.status(201).json(newIngredient)
+})
+
+app.put({})
+
+app.delete({})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+  });
